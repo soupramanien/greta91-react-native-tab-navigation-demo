9269e2fc860e9771231372b90b5adeca1bd79961
soupramanien <soupramani@yahoo.fr>
2023-03-02 16:37:06 +0100

    navigation imbriquée ajoutée

--- App.js
+++ App.js
@@ -1,24 +1,87 @@
-import { Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
+import { Button, Text, View } from 'react-native';
+import { NavigationContainer, useFocusEffect } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { Ionicons } from '@expo/vector-icons'
 
 
-function HomeScreen() {
+function HomeScreen1(props) {
+  useFocusEffect(function () {
+    //cette fonction est appelé lors que l'ecran se trouve au premier plan
+    alert("home screen 1 affiché")
+
+    return function () {
+      //cette fonction est appelé lors que l'ecran perd le focus
+      alert("home screen 1 caché")
+    }
+  })
+  const callback = function () {
+    props.navigation.navigate("Home2")
+  }
+  const callback2 = function () {
+    props.navigation.navigate("Settings", { screen: "Settings2" })
+  }
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Home!</Text>
+      <Text>Home 1!</Text>
+      <Button title='Home 2' onPress={callback} />
+      <Button title='Settings 2' onPress={callback2} />
+    </View>
+  );
+}
+function HomeScreen2(props) {
+  const callback = function () {
+    props.navigation.navigate("Home1")
+  }
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home 2!</Text>
+      <Button title='Home 1' onPress={callback} />
     </View>
   );
 }
 
-function SettingsScreen() {
+function SettingsScreen1(props) {
+  const callback = function () {
+    props.navigation.navigate("Settings2")
+  }
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Settings!</Text>
+      <Text>Settings1!</Text>
+      <Button title='Settings 2' onPress={callback} />
     </View>
   );
 }
+function SettingsScreen2(props) {
+  const callback = function () {
+    props.navigation.navigate("Settings1")
+  }
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Settings2!</Text>
+      <Button title='Settings 1' onPress={callback} />
+    </View>
+  );
+}
+const HomeStack = createNativeStackNavigator()
+function HomeStackScreen() {
+  return (
+    <HomeStack.Navigator>
+      <HomeStack.Screen name="Home1" component={HomeScreen1} />
+      <HomeStack.Screen name="Home2" component={HomeScreen2} />
+    </HomeStack.Navigator>
+  )
+}
+const SettingsStack = createNativeStackNavigator()
+function SettingsStackScreen() {
+  return (
+    <SettingsStack.Navigator>
+      <SettingsStack.Screen name="Settings1" component={SettingsScreen1} />
+      <SettingsStack.Screen name="Settings2" component={SettingsScreen2} />
+    </SettingsStack.Navigator>
+  )
+}
+
 const Tab = createBottomTabNavigator();
 
 export default function App() {
@@ -40,8 +103,16 @@ export default function App() {
           }
         }
       }>
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Settings" component={SettingsScreen} />
+        <Tab.Screen
+          name="Home"
+          component={HomeStackScreen}
+          options={{ headerShown: false }}
+        />
+        <Tab.Screen
+          name="Settings"
+          component={SettingsStackScreen}
+          options={{ headerShown: false }}
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );
