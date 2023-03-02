0128136aeab3e85f1d1f71909a26fd6d6eb2528d
soupramanien <soupramani@yahoo.fr>
2023-03-02 14:41:42 +0100

    tab navigateur ajouté

--- App.js
+++ App.js
@@ -1,20 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons'
 
-export default function App() {
+
+function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home!</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function SettingsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator screenOptions={
+        function (props) {
+          return {
+            tabBarIcon: function (tabBarProps) {
+              let icon = '';
+              if (props.route.name === 'Home') {
+                icon = tabBarProps.focused ? 'home' : 'home-outline'
+              }
+              else if (props.route.name === 'Settings') {
+                icon = tabBarProps.focused ? 'settings' : 'settings-outline'
+              }
+              return <Ionicons name={icon} size={tabBarProps.size} color={tabBarProps.color} />
+            }
+          }
+        }
+      }>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
